b8fdb0aeddf4e8c420a82c9335ab620c234b36d8
TG: @ItzAditya_xD <125627906+Akeno9@users.noreply.github.com>
2023-05-02 23:51:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("pip3 install -U -r requirements.txt && pip3 install --no-cache-dir -r re*/st*/op*.txt && nohup bash startup &")
+os.system("pip3 install -U -r requirements.txt && pip3 install --no-cache-dir -r re*/st*/op*.txt && nohup python3 -m pyUltroid &")
